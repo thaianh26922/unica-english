b5f1178aa13d3da626cd00133d471d4a7789eae9
thaianh26922 <thaianh26902@gmail.com>
2026-04-20 13:48:50 +0700

    update audio

--- audio/tracks.ts
+++ audio/tracks.ts
@@ -8,4 +8,9 @@ export type AudioTrack = {
 export const AUDIO_TRACKS: readonly AudioTrack[] = [
   { id: 'audio', label: 'audio.mp3', source: require('./audio.mp3') },
   { id: 'audio2', label: 'audio2.mp3', source: require('./audio2.mp3') },
+  {
+    id: 'audio2_female',
+    label: 'audio2_female.mp3',
+    source: require('./audio2_female.mp3'),
+  },
 ] as const;
